9dff7b693a633c64aa362a9ff75367cb6cd76e36
Shirley Quintana <46909882+Shirl16@users.noreply.github.com>
2019-07-06 12:39:31 -0600

    final app.py file

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import numpy as np
 from flask import Flask, jsonify, render_template
 from flask_pymongo import PyMongo
 import pymongo
+import json
 
 
 #################################################
@@ -23,118 +24,81 @@ conn = "mongodb://localhost:27017"
 client = pymongo.MongoClient(conn, ConnectTimeoutMS=30000)
 
 # declare database
-# --------------------------------------------------
+#--------------------------------------------------
 db = client.DenverCrime
 coll = db.Population
 crimeColl = db.Crime
 
-
+#  homepage
 @app.route("/")
 def index():
     """Return the homepage """
     return render_template("index.html")
 
 
+
+# page for population results
 @app.route("/population")
 def population():
     Denver_population = coll.find({"NAME": "Denver city"})
-
+    
     print(Denver_population)
-
+   
     pop_mongo_doc = Denver_population[0]
+    print(pop_mongo_doc)
 
-    return jsonify({
-        "NAME": pop_mongo_doc["NAME"],
-        "POPESTIMATE2014": pop_mongo_doc["POPESTIMATE2014"],
-        "POPESTIMATE2015": pop_mongo_doc["POPESTIMATE2015"],
-        "POPESTIMATE2016": pop_mongo_doc["POPESTIMATE2016"],
-        "POPESTIMATE2017": pop_mongo_doc["POPESTIMATE2017"],
-        "POPESTIMATE2018": pop_mongo_doc["POPESTIMATE2018"]
-    })
+    yearList = ["2014", "2015", "2016", "2017", "2018"]
+    popList = [pop_mongo_doc["POPESTIMATE2014"],
+                pop_mongo_doc["POPESTIMATE2015"],
+                pop_mongo_doc["POPESTIMATE2016"],
+                pop_mongo_doc["POPESTIMATE2017"],
+                pop_mongo_doc["POPESTIMATE2018"]
+                ]
+    # print (yearList, popList)
+
+    data = {"year":yearList, "population":popList}
+    # 
+    
+    return jsonify(data)
+   
 
 
-@app.route("/crimes")
-def crimes():
+# page for selected crimes
+@app.route("/crimes/<crime>")
+def crimes(crime):
+
+# murder documents--------------------------------------------------------
+# note: code was written when we were going to grab just murder crimes but then
+# we changed the route/collection find to pick any crime - we decided not to 
+# revise the code below that appears to look for only murders
+
     sampleCrimesMurder = crimeColl.find({
-        "OFFENSE_CATEGORY_ID":"murder",
+        "OFFENSE_CATEGORY_ID":crime
+         })
+    
+
+    murderCrimes_mongo_docs = sampleCrimesMurder
+
+    murder_df = pd.DataFrame([{
+            "OFFENSE_CATEGORY_ID": murderCrimes_mongo_docs["OFFENSE_CATEGORY_ID"],
+            "FIRST_OCCURRENCE_DATE": murderCrimes_mongo_docs["FIRST_OCCURRENCE_DATE"],
+            "GEO_LON": murderCrimes_mongo_docs["GEO_LON"],
+            "GEO_LAT": murderCrimes_mongo_docs["GEO_LAT"]
+
+    } for murderCrimes_mongo_docs in sampleCrimesMurder])
+    
+    murder_df['DATE_YEAR_MONTH'] = pd.to_datetime(murder_df.FIRST_OCCURRENCE_DATE).dt.to_period('Y') 
+    murder_df['DATE_YEAR'] = pd.to_datetime(murder_df.FIRST_OCCURRENCE_DATE).dt.year
+
+    murder_df = murder_df.loc[murder_df['DATE_YEAR']<2019]
+    murder_by_month = murder_df.groupby('DATE_YEAR').count()['OFFENSE_CATEGORY_ID']
+   
+    return jsonify({
+        "year_month": murder_by_month.index.tolist(),
+        "count_murders" : murder_by_month.tolist()
     })
-
-    murder_crimes_mongo_docs = [{
-        "OFFENSE_CATEGORY_ID": murder_crimes_mongo_docs["OFFENSE_CATEGORY_ID"],
-        "INCIDENT_ID": murder_crimes_mongo_docs['INCIDENT_ID'],
-        "FIRST_OCCURRENCE_DATE": murder_crimes_mongo_docs["FIRST_OCCURRENCE_DATE"],
-        "GEO_LON": murder_crimes_mongo_docs["GEO_LON"],
-        "GEO_LAT": murder_crimes_mongo_docs["GEO_LAT"]
-
-    } for murder_crimes_mongo_docs in sampleCrimesMurder]
-
-    sampleCrimesTrafficAccident = crimeColl.find({
-        "OFFENSE_CATEGORY_ID":"traffic-accident",
-    })
-
-    traffic_accident_crimes_mongo_docs = [{
-        "OFFENSE_CATEGORY_ID": traffic_accident_crimes_mongo_docs["OFFENSE_CATEGORY_ID"],
-        "INCIDENT_ID": traffic_accident_crimes_mongo_docs['INCIDENT_ID'],
-        "FIRST_OCCURRENCE_DATE": traffic_accident_crimes_mongo_docs["FIRST_OCCURRENCE_DATE"],
-        "GEO_LON": traffic_accident_crimes_mongo_docs["GEO_LON"],
-        "GEO_LAT": traffic_accident_crimes_mongo_docs["GEO_LAT"]
-
-    } for traffic_accident_crimes_mongo_docs in sampleCrimesTrafficAccident]
-
-    sampleCrimesBurglary = crimeColl.find({
-        "OFFENSE_CATEGORY_ID":"burglary",
-    })
-
-    burglary_crimes_mongo_docs = [{
-        "OFFENSE_CATEGORY_ID": burglary_crimes_mongo_docs["OFFENSE_CATEGORY_ID"],
-        "INCIDENT_ID": burglary_crimes_mongo_docs['INCIDENT_ID'],
-        "FIRST_OCCURRENCE_DATE": burglary_crimes_mongo_docs["FIRST_OCCURRENCE_DATE"],
-        "GEO_LON": burglary_crimes_mongo_docs["GEO_LON"],
-        "GEO_LAT": burglary_crimes_mongo_docs["GEO_LAT"]
-
-    } for burglary_crimes_mongo_docs in sampleCrimesBurglary]
-
-    sampleCrimesDrugAlcohol = crimeColl.find({
-        "OFFENSE_CATEGORY_ID":"drug-alcohol",
-    })
-
-    drug_alcohol_crimes_mongo_docs = [{
-        "OFFENSE_CATEGORY_ID": drug_alcohol_crimes_mongo_docs["OFFENSE_CATEGORY_ID"],
-        "INCIDENT_ID": drug_alcohol_crimes_mongo_docs['INCIDENT_ID'],
-        "FIRST_OCCURRENCE_DATE": drug_alcohol_crimes_mongo_docs["FIRST_OCCURRENCE_DATE"],
-        "GEO_LON": drug_alcohol_crimes_mongo_docs["GEO_LON"],
-        "GEO_LAT": drug_alcohol_crimes_mongo_docs["GEO_LAT"]
-
-    } for drug_alcohol_crimes_mongo_docs in sampleCrimesDrugAlcohol]
-
-    sampleCrimesSexualAssault = crimeColl.find({
-        "OFFENSE_CATEGORY_ID":"sexual-assault",
-    })
-
-    sexual_assault_crimes_mongo_docs = [{
-        "OFFENSE_CATEGORY_ID": sexual_assault_crimes_mongo_docs["OFFENSE_CATEGORY_ID"],
-        "INCIDENT_ID": sexual_assault_crimes_mongo_docs['INCIDENT_ID'],
-        "FIRST_OCCURRENCE_DATE": sexual_assault_crimes_mongo_docs["FIRST_OCCURRENCE_DATE"],
-        "GEO_LON": sexual_assault_crimes_mongo_docs["GEO_LON"],
-        "GEO_LAT": sexual_assault_crimes_mongo_docs["GEO_LAT"]
-
-    } for sexual_assault_crimes_mongo_docs in sampleCrimesSexualAssault]
-
-    sampleCrimesRobbery = crimeColl.find({
-        "OFFENSE_CATEGORY_ID":"robbery",
-    })
-
-    robbery_crimes_mongo_docs = [{
-        "OFFENSE_CATEGORY_ID": robbery_crimes_mongo_docs["OFFENSE_CATEGORY_ID"],
-        "INCIDENT_ID": robbery_crimes_mongo_docs['INCIDENT_ID'],
-        "FIRST_OCCURRENCE_DATE": robbery_crimes_mongo_docs["FIRST_OCCURRENCE_DATE"],
-        "GEO_LON": robbery_crimes_mongo_docs["GEO_LON"],
-        "GEO_LAT": robbery_crimes_mongo_docs["GEO_LAT"]
-
-    } for robbery_crimes_mongo_docs in sampleCrimesRobbery]
-
-    return jsonify(murder_crimes_mongo_docs,traffic_accident_crimes_mongo_docs,burglary_crimes_mongo_docs,burglary_crimes_mongo_docs,drug_alcohol_crimes_mongo_docs,sexual_assault_crimes_mongo_docs,robbery_crimes_mongo_docs)
-
+    
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
